f043a47ad03b6624191288700378fec18e5c1023
Anton Sokolov <onepood@gmail.com>
2022-12-19 11:01:15 +0300

    Slider

--- lib/start.dart
+++ lib/start.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:slide_to_act/slide_to_act.dart';
 
 class Start extends StatelessWidget {
   final VoidCallback startHandler;
@@ -8,29 +9,38 @@ class Start extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Spacer(),
+        Spacer(
+          flex: 1,
+        ),
         Text(
           'Что ты знаешь о русской революции?',
           textAlign: TextAlign.center,
           style: TextStyle(fontSize: 28),
         ),
-        Container(child: Text(
-          'Ровно сто лет назад, в 1917 году, свершилась Октябрьская революция, которая повлияла на ход всей мировой истории. Хорошо ли ты знаешь отечественную историю? Пройди тест, проверь себя!',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 18),
-        ), margin: EdgeInsets.all(12)),
-
-        Image.asset('assets/images/revolution.jpg'),
-        Spacer(),
         Container(
-          child: ElevatedButton(
-            onPressed: startHandler,
-            child: Text('Начать тест'),
-            style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Colors.blue)),
-          ),
-          width: double.infinity,
-          margin: EdgeInsets.all(12),
+            child: Text(
+              'Ровно сто лет назад, в 1917 году, свершилась Октябрьская революция, которая повлияла на ход всей мировой истории. Хорошо ли ты знаешь отечественную историю? Пройди тест, проверь себя!',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18),
+            ),
+            margin: EdgeInsets.all(12)),
+        Image.asset('assets/images/revolution.jpg'),
+        Spacer(flex: 2),
+        Builder(
+          builder: (context) {
+            return Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: SlideAction(
+                child: Text(
+                  'Начать тест',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                sliderButtonIcon: Icon(Icons.lock),
+                outerColor: Colors.blue,
+                onSubmit: startHandler,
+              ),
+            );
+          },
         ),
         Spacer(),
       ],
